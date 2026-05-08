aa850cc23f390349d05a2dfad07c7cde76f20737
Devsebastian44 <zhunaulasebastian31@gmail.com>
2026-05-07 23:06:08 -0500

    ci: update for root domain deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://omnicore-framework.github.io',
-  base: '/omnicore-website',
   vite: {
     plugins: [tailwindcss()],
   },
